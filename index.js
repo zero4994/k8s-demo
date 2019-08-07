e4a496e6ad627be80dae138ab4456de4a6e3d09b
zero4994 <cavc94@gmail.com>
2019-08-07 13:22:06 -0500

    Express app serves vue.js app

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ app.get("/api/hello", (req, res) => {
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.resolve(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
+
 console.log("Starting express server");
 app.listen(port, () => console.log(`App listening on port ${port}!`));
